f4d22675ad32478902c4c646ae0ff710f5cc86ba
Sergey Rushev <rushev84@yandex.ru>
2022-09-10 10:49:01 +0300

    use lib cli for greeting

--- src/Cli.php
+++ src/Cli.php
@@ -2,7 +2,12 @@
 
 namespace BrainGames\Cli;
 
-function hello()
+use function cli\line;
+use function cli\prompt;
+
+function greeting()
 {
-    echo 'Hello from Cli';
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
